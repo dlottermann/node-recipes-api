7a08ee70b4282f7a34763e939cfb8c7a5fc2cd99
Diego <diegolottermann@gmail.com>
2021-01-13 16:26:42 -0300

    feat: add basic script start server
    Basic script to run server on main.js file and add start script on package.json
    - change env.example with PORT

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+
+const dotenv = require('dotenv')
+// const routes = require('../src/routes')
+
+dotenv.config()
+const port = process.env.PORT
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors())
+
+// routes(app)
+
+app.listen(port, function () {
+  console.log('API de Exemplo escutando na porta :' + port)
+})
